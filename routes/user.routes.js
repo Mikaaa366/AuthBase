15e9c24c9f6da6c3985711be3aac789cd6e41090
Dominika Janisz <mikaaa366@gmail.com>
2023-10-04 19:01:23 +0200

    31.4

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+    if(req.user != null) {
+        res.render('logged', { name: req.user.displayName, picture: req.user._json.picture });
+    } else {
+        res.redirect('/no-permission')
+    }
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
+});
+
+router.get('/profile', (req, res) => {
+    if(req.user != null) {
+        res.render('profile');
+    } else {
+        res.redirect('/no-permission')
+    }
+});
+
+router.get('/profile/settings', (req, res) => {
+    if(req.user != null) {
+        res.render('settings');
+    } else {
+        res.redirect('/no-permission')
+    }
 });
 
 
